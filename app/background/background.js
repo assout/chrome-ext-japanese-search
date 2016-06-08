7cf6912c38b004b4922f07822ce081be5058d3b6
assout <assout@users.noreply.github.com>
2016-06-08 23:02:09 +0900

    [WIP]  app/background/background.js | 10 +++++-----  1 file changed, 5 insertions(+), 5 deletions(-)

--- app/background/background.js
+++ app/background/background.js
@@ -1,13 +1,13 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  if(statusManager.getStatus()) {
+  if (statusManager.getStatus()) {
     chrome.browserAction.setIcon({path:"../icon/icon-19-off.png"});
   } else {
     chrome.browserAction.setIcon({path:"../icon/icon-19-on.png"});
   }
   statusManager.changeStatus();
 
-  if(urlManager.isGoogle(tab.url)) {
-    if(statusManager.getStatus()) {
+  if (urlManager.isGoogle(tab.url)) {
+    if (statusManager.getStatus()) {
       chrome.tabs.update(tab.id, {url:tab.url + "&lr=lang_ja"});
     } else {
       var url = tab.url;
@@ -17,8 +17,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   }
 });
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if(statusManager.getStatus()) {
-    if(urlManager.isGoogle(tab.url) && !urlManager.getParam(tab.url, "lr") && !urlManager.getParam(tab.url, "url")) {
+  if (statusManager.getStatus()) {
+    if (urlManager.isGoogle(tab.url) && !urlManager.getParam(tab.url, "lr") && !urlManager.getParam(tab.url, "url")) {
       chrome.tabs.update(tabId, {url:tab.url + "&lr=lang_ja"});
     }
   }
